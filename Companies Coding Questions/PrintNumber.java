e39126492653e0dab4348ac00b4839dfea8cfc81
Iamdheeraj21 <dheerajprajapati334@gmail.com>
2021-08-07 23:01:36 +0530

    Print number with out any loop

---
+++ Companies Coding Questions/PrintNumber.java
@@ -0,0 +1,12 @@
+
+public class PrintNumber {
+    public static void main(String[] args) {
+        printNumber(1);
+    }
+    public static void printNumber(int n) {
+        if(n<=10){
+            System.out.println(n);
+            printNumber(n+1);
+        }
+    }
+}
