4177c2a42a11869e2ff02e4a86d93b69f722cb2b
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2021-10-03 03:31:48 +0530

    Update Code

---
+++ Loops/Loop1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Loop1
+{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        for (int i = 0; i <n; i++) {
+            for(int j=0;j<j;j++){
+                System.out.println("*");
+            }
+            System.out.println("\n");
+        }
+        scanner.close();
+    }
+}
